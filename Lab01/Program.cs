343d1a0321f236f86f45460437cb6974e37ffb7c
valdezx <orom1331@gmail.com>
2026-09-08 18:26:34 +0300

    Lab01: Task2

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+namespace Lab01
+{
+    internal class Program
+    {
+
+    private static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+    System.Globalization.CultureInfo.InvariantCulture;
+            Task1.Run();
+            Task2.Run();
+        }
+}
+}
