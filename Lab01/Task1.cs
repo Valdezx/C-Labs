ca5b733fae9638bfb3a0e9709f4debdcbf5c05d2
valdezx <orom1331@gmail.com>
2026-09-08 17:06:15 +0300

    Lab01:   Task1   done

---
+++ Lab01/Task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01
+{
+    internal class Task1
+    {
+
+        public static void Run()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+    System.Globalization.CultureInfo.InvariantCulture;
+            double weight;
+            double height;
+            double IMT;
+            Console.Write("Введіть вашу вагу (кг): ");
+            weight = double.Parse(Console.ReadLine());
+            Console.Write("Введіть ваш зріст (м): ");
+            height = double.Parse(Console.ReadLine());
+            IMT = weight / (height * height);
+            Console.WriteLine($"Ваш індекс маси тіла: {IMT:F2}");
+        }
+    }
+}
