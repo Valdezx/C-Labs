cc2c99504a928bfce07d636ea8581f76aff790e0
valdezx <orom1331@gmail.com>
2026-09-10 16:23:44 +0300

    Lab01:Task4

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -12,6 +12,7 @@ namespace Lab01
             Task1.Run();
             Task2.Run();
             Task3.Run();
+            Task4.Run();
         }
 }
 }
---
+++ Lab01/Task4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01
+{
+    internal class Task4
+    {
+        public static void Run()
+        {
+            int DiastolicPressure = 0;
+            int SystolicPressure = 0;
+            string[] categories = { "Норма", "Підвищений", "Гіпертонія 1 ступеня", "Гіпертонія 2 ступеня" };
+            Console.WriteLine("Введіть діастолічний тиск: ");
+            DiastolicPressure = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть систолічний тиск:  ");
+            SystolicPressure = int.Parse(Console.ReadLine());
+            if (SystolicPressure <120 &&  DiastolicPressure<= 80)
+            {
+                Console.WriteLine("Ви належите до категорії: " + categories[0]);
+            }
+            else if (SystolicPressure >= 120 && SystolicPressure < 130 && DiastolicPressure >= 80 && DiastolicPressure < 85)
+            {
+                Console.WriteLine("Ви належите до категорії: " + categories[1]);
+            }
+            else if (SystolicPressure >= 130 && SystolicPressure < 140 && DiastolicPressure >= 85 && DiastolicPressure < 90)
+            {
+                Console.WriteLine("Ви належите до категорії: " + categories[2]);
+            }
+            else
+            {
+                Console.WriteLine("Ви належите до категорії: " + categories[3]);
+            }
+        }
+    }
+}
